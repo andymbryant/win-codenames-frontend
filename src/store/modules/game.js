75c78f6f5a7aeb3d06fe2b8f750f75ecf0a3ef1a
Andy Bryant <a.mcdermid.bryant@gmail.com>
2020-08-02 16:36:03 -0400

    working on api

--- src/store/modules/game.js
+++ src/store/modules/game.js
@@ -1,4 +1,7 @@
 import {shuffle} from '@/helpers.js'
+import axios from 'axios'
+
+const ROOT_URL = process.env.VUE_APP_FLASK
 
 function getDefaultState() {
   return {
@@ -12,15 +15,20 @@ const state = getDefaultState()
 const getters = {
   cards: state => state.cards,
   spymaster: state => state.spymaster
-};
+}
 
 const actions = {
   async fetchGameData({ commit }) {
-    const data = require(`../../data/round1.json`);
-    commit('loadGameData', data)
+    const url = `${ROOT_URL}/games/`
+    const game = await axios.get(url)
+
+    return axios.get(url, {headers: {Authorization: `${store.getters.authHeaderValue}`}})
+    console.error(error)
+    commit('loadGameData', game)
   }
 }
 
+// const game = require(`../../data/round1.json`);
 const mutations = {
   loadGameData(state, {cards, spymaster}) {
     state.cards = shuffle(cards)
